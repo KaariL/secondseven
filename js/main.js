e58dc630cc0a29e8ffe3a7820c8fabece50c555c
KaariL <kaari.landry@gmail.com>
2017-05-03 18:32:35 -0300

    form and rid margin collapse of services
    collapse fix doesn’t work on resize

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ $.ajaxPrefilter( function (options) {
     //options.url = "http://cors.corsproxy.io/url=" + options.url;
   }
 });
+
+
+//PREVENT MARGIN COLLAPSE ON SERVICES CONTAINERS OF  INDEX
+
+
+  if ($(window).width() < 768) {
+      // window width is at less than 768px
+
+      var currentHeight = $('.services').height();
+      var newHeight = currentHeight + 120 + 'px';
+      console.log(newHeight);
+      function setNewHeight() {
+        console.log('newHeight');
+        $('.services').css('height', newHeight );
+      };
+      setNewHeight();
+}
+else {
+    // window width is equal or greater than 768px
+}
