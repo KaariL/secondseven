d0fa2cfe030c3c5d211e2835de428463764e4571
KaariL <kaari.landry@gmail.com>
2017-05-05 22:47:49 -0300

    bg video w/ pattern

--- js/main.js
+++ js/main.js
@@ -11,18 +11,15 @@ $.ajaxPrefilter( function (options) {
 //PREVENT MARGIN COLLAPSE ON SERVICES CONTAINERS OF  INDEX
 
 
+
   if ($(window).width() < 768) {
       // window width is at less than 768px
-
       var currentHeight = $('.services').height();
       var newHeight = currentHeight + 120 + 'px';
-      console.log(newHeight);
+
+
       function setNewHeight() {
-        console.log('newHeight');
         $('.services').css('height', newHeight );
       };
       setNewHeight();
-}
-else {
-    // window width is equal or greater than 768px
-}
+} 
