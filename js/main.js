a4d756915482547d9577d488bbba6a3740ca9d73
KaariL <kaari.landry@gmail.com>
2017-05-18 12:56:53 -0300

    ready to upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
-//HTML INCLUDES HTML INCLUDES HTML INCLUDES HTML INCLUDES HTML INCLUDES HTML INCLUDES HTML INCLUDES
-$.ajaxPrefilter( function (options) {
-  if (options.crossDomain && jQuery.support.cors) {
-    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-    //options.url = "http://cors.corsproxy.io/url=" + options.url;
-  }
-});
 
 
 //PREVENT MARGIN COLLAPSE ON SERVICES CONTAINERS OF  INDEX
-
-
-
   if ($(window).width() < 768) {
       // window width is at less than 768px
       var currentHeight = $('.services').height();
@@ -24,6 +13,17 @@ $.ajaxPrefilter( function (options) {
       //currently doesn't adjust on window resize
 }
 
+//LINKS FROM SERVICES TO ACCORDIAN WITH CORRECT SUBJECT active
+//$('.services-container').click(function(){
+//  var i = $('.services').indexOf(this);
+//  $('#services li').removeClass('active');
+//  $('#services li').eq(i).addClass('active');
+//  console.log(i);
+//});
+//select a service with index=i from the home page
+//open the services page
+//select the li with that index for class active
+//select the services section and give it class selected
 
 //ACCORDIAN ACCORDIAN ACCORDIAN ACCORDIAN ACCORDIAN
 
